b57d1c6171ba9e7c16cb392bdf4b1303ad82ddd7
Andrey Sheremeev <sheremeev.andrey@gmail.com>
2022-03-14 00:55:21 +0300

    some changes and steps to create fragment tabs

---
+++ app/src/main/java/com/example/model/MyAdapter.java
@@ -0,0 +1,41 @@
+package com.example.model;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentManager;
+
+public class MyAdapter extends FragmentPagerAdapter {
+
+    private Context myContext;
+    int totalTabs;
+
+    public MyAdapter(Context context, FragmentManager fm, int totalTabs) {
+        super(fm);
+        myContext = context;
+        this.totalTabs = totalTabs;
+    }
+
+    // this is for fragment tabs
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                PhotoWindowFragment photoWindowFragment = new PhotoWindowFragment();
+                return photoWindowFragment;
+            case 1:
+                PlateWindowFragment plateWindowFragment = new PlateWindowFragment();
+                return plateWindowFragment;
+            case 2:
+                 MainWindowFragment mainWindowFragment= new MainWindowFragment();
+                return mainWindowFragment;
+            default:
+                return null;
+        }
+    }
+    // this counts total number of tabs
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+}
