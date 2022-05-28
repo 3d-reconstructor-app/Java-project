af13244db00955ba2e14f64798c9aefd0add2a84
Slava314 <vorobev.stu@gmail.com>
2022-05-28 13:31:33 +0300

    fix files, add exceptions

--- as/app/src/main/java/com/example/a3dmodel/Client.java
+++ as/app/src/main/java/com/example/a3dmodel/Client.java
@@ -1,13 +1,15 @@
 package com.example.a3dmodel;
 
 import java.io.*;
+
 import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import java.nio.file.Paths;
+import okhttp3.ResponseBody;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -18,19 +20,29 @@ public class Client {
     private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
     private static final MediaType IMAGE = MediaType.get("image/png");
 
-    private static void initializationHTTPPOSTRequest(OkHttpClient client, String url, String token, long numberOfFiles) throws IOException, JSONException {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("id", token);
-        jsonObject.put("count", numberOfFiles); // size of dir or number of selected
-        try {
-            FileWriter file = new FileWriter("test/ini.json");
-            file.write(jsonObject.toString());
-            file.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void HttpClientRequest(List<File> files, File result) throws AppException {
+        String token = UUID.randomUUID().toString();
+        String url = "http://localhost:8000/";
+        System.out.println(token);
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        initializationHTTPPOSTRequest(client, url, token, files.size());
+        for (File file : files) {
+            sendFileHTTPPOSTRequest(client, url, token, file);
         }
 
-        RequestBody body = RequestBody.create(data, JSON);
+        getFileHTTPGETRequest(client, url, token, result);
+    }
+
+    private static void initializationHTTPPOSTRequest(OkHttpClient client, String url, String token, long numberOfFiles) throws AppException {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("id", token);
+            jsonObject.put("count", numberOfFiles);
+        } catch (JSONException e) {
+            throw new AppException("Can't make json to initialize reconstruction", e);
+        }
+
+        RequestBody body = RequestBody.create(jsonObject.toString(), JSON);
         Request requestIni = new Request.Builder()
                 .url(HttpUrl.get(url + token + ".json"))
                 .post(body)
@@ -41,50 +53,43 @@ public class Client {
             if (response.isSuccessful()) {
                 System.out.println("Send initialization: Ok");
             }
+        } catch (IOException e) {
+            throw new AppException("Can't make initialization request", e);
         }
 
     }
 
-    private static void sendFileHTTPPOSTRequest(OkHttpClient client, String url, String token, String filePath) throws IOException {
-        RequestBody body = RequestBody.create(data, IMAGE);
+    private static void sendFileHTTPPOSTRequest(OkHttpClient client, String url, String token, File file) throws AppException {
+        RequestBody body = RequestBody.create(file, IMAGE);
         Request requestIni = new Request.Builder()
-                .url(HttpUrl.get(url + token + "/images/" + Paths.get(filePath).getFileName().toString()))
+                .url(HttpUrl.get(url + token + "/images/" + file.getName()))
                 .post(body)
                 .header("Content-Type", "image/png")
                 .build();
         try (Response response = client.newCall(requestIni).execute()) {
             if (response.isSuccessful()) {
-                System.out.println("Send initialization: Ok");
+                System.out.println("Send file " + file.getName() + " : Ok");
             }
+        } catch (IOException e) {
+            throw new AppException("Can't end file " + file.getName(), e);
         }
     }
 
-    private static void getFileHTTPGETRequest(OkHttpClient client, String url, String token, File result) throws IOException, InterruptedException {
+    private static void getFileHTTPGETRequest(OkHttpClient client, String url, String token, File result) throws AppException {
         Request requestGet = new Request.Builder()
                 .url(HttpUrl.get(url + "resources/" + token + "/reconstruction_sequential/PMVS/models/pmvs_options.txt.ply"))
                 .build();
         try (Response response = client.newCall(requestGet).execute()) {
             if (response.isSuccessful()) {
-                FileOutputStream stream = new FileOutputStream(result);
-                try {
-                    stream.write(response.body().byteStream().read());
-                } finally {
-                    stream.close();
+                try (FileOutputStream stream = new FileOutputStream(result)) {
+                    ResponseBody body = response.body();
+                    if (body != null) {
+                        stream.write(body.byteStream().read());
+                    }
                 }
             }
+        } catch (IOException e) {
+            throw new AppException("Can't get result", e);
         }
-    }
-
-    public static void HttpClientRequest(List<String> files) throws IOException, InterruptedException, JSONException {
-        String token = UUID.randomUUID().toString();
-        String url = "http://localhost:8000/";
-        System.out.println(token);
-        OkHttpClient client = new OkHttpClient.Builder().build();
-        initializationHTTPPOSTRequest(client, url, token, files.size());
-        for (String file : files) {
-            sendFileHTTPPOSTRequest(client, url, token, file);
-        }
-
-        getFileHTTPGETRequest(client, url, token, "test/pmvs_options.txt.ply");
     }
 }
