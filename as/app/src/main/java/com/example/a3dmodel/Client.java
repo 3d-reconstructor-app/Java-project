df46e3919333ee0f30bf0f6fdc49481654a7abfe
Slava314 <vorobev.stu@gmail.com>
2022-06-13 15:16:53 +0300

    fix and test okhttp

--- as/app/src/main/java/com/example/a3dmodel/Client.java
+++ as/app/src/main/java/com/example/a3dmodel/Client.java
@@ -20,11 +20,11 @@ public class Client {
     private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
     private static final MediaType IMAGE = MediaType.get("image/png");
 
-    public static void HttpClientRequest(List<File> files, File result) throws AppException {
+    public static void httpClientRequest(List<File> files, File result) throws AppException {
         String token = UUID.randomUUID().toString();
-        String url = "http://localhost:8000/";
+        String url = "http://127.0.0.1:8000/";
         System.out.println(token);
-        OkHttpClient client = new OkHttpClient.Builder().build();
+        OkHttpClient client = new OkHttpClient();
         initializationHTTPPOSTRequest(client, url, token, files.size());
         for (File file : files) {
             sendFileHTTPPOSTRequest(client, url, token, file);
@@ -52,6 +52,8 @@ public class Client {
         try (Response response = client.newCall(requestIni).execute()) {
             if (response.isSuccessful()) {
                 System.out.println("Send initialization: Ok");
+            } else {
+                throw new AppException("Error response");
             }
         } catch (IOException e) {
             throw new AppException("Can't make initialization request", e);
@@ -69,27 +71,41 @@ public class Client {
         try (Response response = client.newCall(requestIni).execute()) {
             if (response.isSuccessful()) {
                 System.out.println("Send file " + file.getName() + " : Ok");
+            } else {
+                throw new AppException("Error response");
             }
         } catch (IOException e) {
-            throw new AppException("Can't end file " + file.getName(), e);
+            throw new AppException("Can't send file " + file.getName(), e);
         }
     }
 
     private static void getFileHTTPGETRequest(OkHttpClient client, String url, String token, File result) throws AppException {
         Request requestGet = new Request.Builder()
+//                .url(HttpUrl.get(url + "resources/" + token + "/res.png"))
                 .url(HttpUrl.get(url + "resources/" + token + "/reconstruction_sequential/PMVS/models/pmvs_options.txt.ply"))
+                .get()
                 .build();
         try (Response response = client.newCall(requestGet).execute()) {
             if (response.isSuccessful()) {
                 try (FileOutputStream stream = new FileOutputStream(result)) {
                     ResponseBody body = response.body();
                     if (body != null) {
-                        stream.write(body.byteStream().read());
+                        stream.write(body.bytes());
                     }
                 }
+            } else {
+                throw new AppException("Error response");
             }
         } catch (IOException e) {
             throw new AppException("Can't get result", e);
         }
     }
+
+//    public static void main(String[] args) throws AppException, IOException {
+//        File res = new File("test/res.png");
+//        List<File> files = new ArrayList<>();
+//        files.add(new File("test/im1.png"));
+//        files.add(new File("test/im2.png"));
+//        httpClientRequest(files, res);
+//    }
 }
