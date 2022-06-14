99446feb6045dc809275ddf40e162e33989d0f13
Slava314 <vorobev.stu@gmail.com>
2022-06-14 20:51:27 +0300

    add zip archive of photos

--- as/app/src/main/java/com/example/a3dmodel/Client.java
+++ as/app/src/main/java/com/example/a3dmodel/Client.java
@@ -4,18 +4,25 @@ import java.io.*;
 
 import okhttp3.*;
 
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Client {
-    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
-    private static final MediaType IMAGE = MediaType.get("image/png");
+    private static final String IMAGE_STRING = "image/png";
+    private static final String ZIP_STRING = "application/zip";
+    private static final String JSON_STRING = "application/json";
+    private static final MediaType JSON = MediaType.get(JSON_STRING);
+    private static final MediaType IMAGE = MediaType.get(IMAGE_STRING);
+    private static final MediaType ZIP = MediaType.get(ZIP_STRING);
 
     public static void httpClientRequest(List<File> files, File result) throws AppException, IOException {
         String token = UUID.randomUUID().toString();
@@ -35,19 +42,36 @@ public class Client {
                     }
                 })
                 .build();
-        initializationHTTPPOSTRequest(client, url, token, files.size());
-        for (File file : files) {
-            sendFileHTTPPOSTRequest(client, url, token, file);
-        }
 
+        File zipFile = Files.createTempFile(null, ".zip").toFile();
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFile));
+        for (File file : files) {
+            ZipEntry e = new ZipEntry(file.getName());
+            FileInputStream fin = new FileInputStream(file);
+            out.putNextEntry(e);
+            int length;
+            byte[] buffer = new byte[1024];
+            while ((length = fin.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+            out.closeEntry();
+        }
+        out.close();
+        System.out.println(files.stream().mapToLong(File::length).sum());
+        System.out.println(zipFile.length());
+
+
+        initializationHTTPPOSTRequest(client, url, token, 1, zipFile.getName());
+        sendFileHTTPPOSTRequest(client, url, token, zipFile, ZIP);
         getFileHTTPGETRequest(client, url, token, result);
     }
 
-    private static void initializationHTTPPOSTRequest(OkHttpClient client, String url, String token, long numberOfFiles) throws AppException {
+    private static void initializationHTTPPOSTRequest(OkHttpClient client, String url, String token, long numberOfFiles, String zipFileName) throws AppException {
         JSONObject jsonObject = new JSONObject();
         try {
             jsonObject.put("id", token);
             jsonObject.put("count", numberOfFiles);
+            jsonObject.put("name", zipFileName);
         } catch (JSONException e) {
             throw new AppException("Can't make json to initialize reconstruction", e);
         }
@@ -56,7 +80,6 @@ public class Client {
         Request requestIni = new Request.Builder()
                 .url(HttpUrl.get(url + token + ".json"))
                 .post(body)
-                .header("Content-Type", "application/json")
                 .build();
 
         try (Response response = client.newCall(requestIni).execute()) {
@@ -69,12 +92,11 @@ public class Client {
 
     }
 
-    private static void sendFileHTTPPOSTRequest(OkHttpClient client, String url, String token, File file) throws AppException, IOException {
-        RequestBody body = RequestBody.create(file, IMAGE);
+    private static void sendFileHTTPPOSTRequest(OkHttpClient client, String url, String token, File file, MediaType mediaType) throws AppException {
+        RequestBody body = RequestBody.create(file, mediaType);
         Request requestIni = new Request.Builder()
                 .url(HttpUrl.get(url + token + "/images/" + file.getName()))
                 .post(body)
-                .header("Content-Type", "image/png")
                 .build();
         try (Response response = client.newCall(requestIni).execute()) {
             if (response.isSuccessful()) {
@@ -87,8 +109,8 @@ public class Client {
 
     private static void getFileHTTPGETRequest(OkHttpClient client, String url, String token, File result) throws AppException {
         Request requestGet = new Request.Builder()
-                .url(HttpUrl.get(url + "resources/" + token + "/res.png"))
-//                .url(HttpUrl.get(url + "resources/" + token + "/reconstruction_sequential/PMVS/models/pmvs_options.txt.ply"))
+//                .url(HttpUrl.get(url + "resources/" + token + "/res.png"))
+                .url(HttpUrl.get(url + "resources/" + token + "/reconstruction_sequential/PMVS/models/pmvs_options.txt.ply"))
                 .get()
                 .build();
         try (Response response = client.newCall(requestGet).execute()) {
@@ -98,6 +120,7 @@ public class Client {
                     if (body != null) {
                         stream.write(body.bytes());
                     }
+                    System.out.println("Get file " + result.getName() + " : Ok");
                 }
             } else {
                 System.out.println("wtf");
